84bd7392c9785233adca66329760e38de541bf64
revus05 <skninni05@gmail.com>
2026-03-01 08:35:33 +0300

    initial commit

---
+++ backend/src/main/java/com/collapse/collapse_storage/CollapseStorageApplication.java
@@ -0,0 +1,11 @@
+package com.collapse.collapse_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollapseStorageApplication {
+	static void main(String[] args) {
+		SpringApplication.run(CollapseStorageApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/collapse/collapse_storage/CollapseStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collapse.collapse_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollapseStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
